308e0aabebfc1123ac73f6d0423963e24959d6e5
Aleksey Voznosimenko <avoznosimenko@qualium-systems.com>
2017-01-04 22:42:05 +0200

    new armor - man

--- src/collaborative/src/app/modules/char-editor/components/armor-visual/armor-visual.component.ts
+++ src/collaborative/src/app/modules/char-editor/components/armor-visual/armor-visual.component.ts
@@ -14,10 +14,20 @@ export class ArmorVisualComponent implements OnInit {
     private current: CurrentCharService
   ) { }
 
-  dr_description(location: string, front: boolean) {
-    let side = front ? 'Front' : 'Rear';
-    let {dr, ep, epi} = this.current.char.damageResistance[location][side.toLowerCase()];
-    return `${side}: ${dr}/${ep}/${epi}`;
+  // dr_description(location: string, front: boolean) {
+  //   let side = front ? 'Front' : 'Rear';
+  //   let {dr, ep, epi} = this.current.char.damageResistance[location][side.toLowerCase()];
+  //   return `${side}: ${dr}/${ep}/${epi}`;
+  // }
+
+
+
+  dr_caption(location: string) {
+    let dr = this.current.char.damageResistance[location];
+    function format(block): string {
+      return `${block.dr} / ${block.ep} / ${block.epi}`;
+    }
+    return `F:${format(dr.front)} R:${format(dr.rear)}`;
   }
 
   ngOnInit() {
